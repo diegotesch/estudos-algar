57d203238dd43e333050ef808a3585f47a20adcd
Diego Tesch Gramelich <diego.tesch@gmail.com>
2019-11-22 15:33:59 -0300

    Event Binding intro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nome = "Diego";
+
+  adicionar() {
+    console.log(`Adicionando ${this.nome}`);
+
+    const numero = Math.round(Math.random() * 100);
+    this.nome = 'Diego '+numero;
+  }
+
+  alterarNome(event: any){
+    // console.log(event.target.value);
+    this.nome = event.target.value
+  }
+
 }
