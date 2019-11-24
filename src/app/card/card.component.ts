56988e42651e907f8adb264e18089d80e53e1051
Diego Tesch Gramelich <diego.tesch@gmail.com>
2019-11-24 11:10:45 -0300

    ngStyle e ngClass

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -9,4 +9,18 @@ export class CardComponent {
 
   @Input() card : any
 
+  getCardStyle() {
+    return {
+      borderWidth : '2pxx',
+      backgroundColor : this.card.id  % 2 ? 'lightblue' : 'lightgreen'
+    };
+  }
+
+  // getListaClassesCss() {
+  //   return ['badge', 'badge-success'];
+  // }
+
+  isAdmin() {
+    return this.card.nome.startsWith('D');
+  }
 }
